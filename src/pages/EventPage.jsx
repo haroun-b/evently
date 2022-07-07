bca13cfedebf602391622343a24947067951b4d4
Haroun B <haroun@harryb.dev>
2022-07-07 11:24:45 +0200

    adds style and navigation to eventPage

--- src/pages/EventPage.jsx
+++ src/pages/EventPage.jsx
@@ -1,10 +1,12 @@
 import React, { useEffect, useState } from "react";
-import AttendActionBar from "../components/AttendActionBar";
+import { useNavigate, useParams } from "react-router-dom";
 import axiosInstance from "../utils/axiosInstance";
 
+import { Avatar, AvatarGroup, Container, IconButton, Stack, Typography } from "@mui/material";
+import { CloseRounded } from "@mui/icons-material";
+
 import "./styles/EventPage.css";
-import { useNavigate, useParams } from "react-router-dom";
-import { Avatar, AvatarGroup, Box, Container, Stack, Typography } from "@mui/material";
+import AttendActionBar from "../components/AttendActionBar";
 
 
 const EventPage = ({ currentUser }) => {
@@ -70,7 +72,9 @@ const EventPage = ({ currentUser }) => {
       })
   }
 
-  const openChat = () => { };
+  const openChat = () => {
+    navigate(`/events/${id}/chat`)
+  };
 
   const handlers = { handleAttend, handleCancel, openChat };
 
@@ -80,118 +84,130 @@ const EventPage = ({ currentUser }) => {
 
   return (
     <>
-    <Stack
-      sx={{
-        textAlign: 'left',
-        marginBottom: '5rem'
-      }}
-    >
-      <Stack className="topActionBar">
-
-      </Stack>
-
-      <img
-        src={event.imageUrl}
-        alt={event.title}
-        style={{
-          height: '5%',
-          marginBottom: '1rem'
-        }}
-      />
-
-      <Container>
-        <Typography variant="h4" component="h1">
-          {event.title}
-        </Typography>
-
-        <Typography variant="subtitle2" component="h2">
-          {`From: ${event.startAt.slice(0, -8)}`}
-        </Typography>
-        <Typography variant="subtitle2" component="h2">
-          {`To: ${event.endAt.slice(0, -8)}`}
-        </Typography>
-
-        <Typography variant="subtitle1" component="h2">
-          {`${event.address.street} - ${event.address.city}`}
-        </Typography>
-      </Container>
-
-      <Container>
-        <Typography variant="h6" component="h2">
-          Description:
-        </Typography>
-
-        <Typography variant="body1">
-          {event.description}
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur aspernatur facere asperiores alias, laborum fuga nisi enim ipsa reiciendis quod inventore et deserunt cum soluta iste ab reprehenderit dolor pariatur.
-        </Typography>
-      </Container>
-
-      <Stack className="eventParticipents"
-        direction="row"
-        justifyContent="center"
-        alignItems="center"
-        spacing={0}
+      <IconButton
+        aria-label="close"
         sx={{
+          position: 'absolute',
+          top: '.2rem',
+          right: '.2rem',
+          zIndex: 1,
+          color: '#000'
+        }}
+        onClick={() => {navigate('..')}}
+      >
+        <CloseRounded />
+      </IconButton>
+
+      <Stack
+        sx={{
+          textAlign: 'left',
+          marginBottom: '5rem',
           marginTop: '2rem'
         }}
       >
-        <Stack
-          direction="column"
-          justifyContent="flex-start"
+        <img
+          src={event.imageUrl}
+          alt={event.title}
+          style={{
+            height: '5%',
+            marginBottom: '1rem'
+          }}
+        />
+
+        <Container>
+          <Typography variant="h4" component="h1">
+            {event.title}
+          </Typography>
+
+          <Typography variant="subtitle2" component="h2">
+            {`From: ${event.startAt.slice(0, -8)}`}
+          </Typography>
+          <Typography variant="subtitle2" component="h2">
+            {`To: ${event.endAt.slice(0, -8)}`}
+          </Typography>
+
+          <Typography variant="subtitle1" component="h2">
+            {`${event.address.street} - ${event.address.city}`}
+          </Typography>
+        </Container>
+
+        <Container>
+          <Typography variant="h6" component="h2">
+            Description:
+          </Typography>
+
+          <Typography variant="body1">
+            {event.description || 'Nothing...'}
+          </Typography>
+        </Container>
+
+        <Stack className="eventParticipents"
+          direction="row"
+          justifyContent="center"
           alignItems="center"
+          spacing={0}
           sx={{
-            width: '50%',
-            height: '5rem',
-            padding: '.3rem',
-            backgroundColor: '#fff',
-            '&:hover': {
-              backgroundColor: '#bfbfbf',
-              opacity: [0.9, 0.8, 0.7],
-            },
+            marginTop: '2rem'
           }}
         >
-          <Typography variant="h6" component="h2">
-            Creator
-          </Typography>
-          <Avatar
-            alt={event.creator.name}
-            src={event.creator.imageUrl}
-          />
-        </Stack>
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+            sx={{
+              width: '50%',
+              height: '5rem',
+              padding: '.3rem',
+              backgroundColor: '#fff',
+              '&:hover': {
+                backgroundColor: '#bfbfbf',
+                opacity: [0.9, 0.8, 0.7],
+              }
+            }}
+            onClick={() => {navigate(`/users/${event.creator.username}`)}}
+          >
+            <Typography variant="h6" component="h2">
+              Creator
+            </Typography>
+            <Avatar
+              alt={event.creator.name}
+              src={event.creator.imageUrl}
+            />
+          </Stack>
 
-        <Stack
-          direction="column"
-          justifyContent="flex-start"
-          alignItems="center"
-          sx={{
-            width: '50%',
-            height: '5rem',
-            padding: '.3rem',
-            backgroundColor: '#fff',
-            '&:hover': {
-              backgroundColor: '#bfbfbf',
-              opacity: [0.9, 0.8, 0.7],
-            },
-          }}
-        >
-          <Typography variant="h6" component="h2">
-            Attendees
-          </Typography>
-          <AvatarGroup max={4}>
-            {(() => {
-              const attendees = event.attendees.requests || event.attendees.approved;
+          <Stack
+            direction="column"
+            justifyContent="flex-start"
+            alignItems="center"
+            sx={{
+              width: '50%',
+              height: '5rem',
+              padding: '.3rem',
+              backgroundColor: '#fff',
+              '&:hover': {
+                backgroundColor: '#bfbfbf',
+                opacity: [0.9, 0.8, 0.7],
+              },
+            }}
+            onClick={() => { navigate(`/events/${id}/attendees`)}}
+          >
+            <Typography variant="h6" component="h2">
+              Attendees
+            </Typography>
+            <AvatarGroup max={4}>
+              {(() => {
+                const attendees = event.attendees.requests || event.attendees.approved;
 
-              return attendees.map(attendee =>
-                <Avatar alt={attendee.user.name} src={attendee.user.imageUrl} key={attendee.user.id} />
-              )
-            })()}
-          </AvatarGroup>
+                return attendees.map(attendee =>
+                  <Avatar alt={attendee.user.name} src={attendee.user.imageUrl} key={attendee.user.id} />
+                )
+              })()}
+            </AvatarGroup>
+          </Stack>
         </Stack>
       </Stack>
-    </Stack>
 
-    <Stack className="eventParticipents"
+      <Stack className="eventParticipents"
         direction="row"
         justifyContent="space-around"
         alignItems="center"
@@ -214,7 +230,7 @@ const EventPage = ({ currentUser }) => {
           attendanceStatus={event.myStatus}
         />
       </Stack>
-      </>
+    </>
   );
 };
 
